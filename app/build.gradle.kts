08ceb1bbfa7209be487b023cf3fd17c43fa88cff
reverti <hermannzosou91@gmail.com>
2024-07-07 17:17:36 +0700

    Architecture app and getting catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
 
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.lifecycle.runtime.ktx)
-	implementation(libs.androidx.coil.kt)
+	//implementation(libs.androidx.coil.kt)
 
 	// Compose
 	implementation(libs.androidx.activity.compose)
