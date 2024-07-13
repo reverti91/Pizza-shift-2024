6e4e1c418d154079d22a54f42738f616e0d85f92
reverti <hermannzosou91@gmail.com>
2024-07-13 22:28:36 +0700

    Architecture app and getting catalog

---
+++ app/src/main/java/com/shift2024/revertisoft/catalog_pizza/domain/entity/PizzaSize.kt
@@ -0,0 +1,8 @@
+package com.shift2024.revertisoft.catalog_pizza.domain.entity
+
+import com.shift2024.revertisoft.common.domain.PizzaSizeEnum
+
+class PizzaSize (
+val name: PizzaSizeEnum,
+val price:Long,
+)
